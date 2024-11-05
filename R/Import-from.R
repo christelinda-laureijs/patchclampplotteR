3986b7305eda6bef0c981ce0ed6c96a7f05ef258
christelinda-laureijs <cslaureijs@gmail.com>
2024-11-04 22:48:59 -0400

    Added ORCID and updated author information. Fixed error with dataui package not being recognized as an imported package.

--- R/Import-from.R
+++ R/Import-from.R
@@ -6,3 +6,11 @@ magrittr::`%>%`
 
 #' @importFrom rlang .data
 rlang::.data
+
+
+# Required to use dataui package which is suggested by reactablefmtr
+# https://r-pkgs.org/dependencies-in-practice.html#how-to-not-use-a-package-in-imports
+dataui_function <- function() {
+  dataui::dui_sparkline()
+
+}
