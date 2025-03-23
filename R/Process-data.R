58397859db0a022953c2e79faf0480f9f4350049
christelinda-laureijs <cslaureijs@gmail.com>
2025-03-23 13:10:15 -0300

    Kept p-values in t-test

--- R/Process-data.R
+++ R/Process-data.R
@@ -1339,6 +1339,7 @@ perform_t_tests_for_summary_plot <- function(data,
       .data$statistic,
       .data$df,
       .data$p_string,
+      .data$p.adj,
       .data$significance_stars,
       .data$asterisk_time
     ) %>%
